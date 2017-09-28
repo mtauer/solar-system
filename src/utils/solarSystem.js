cf17ee51382fa170a3b60211ca6e1067b846341c
Michael Tauer <michael.tauer@futurice.com>
2017-09-28 23:27:53 +0200

    Create solar system utils class; Add usage examples for SolarSystem util

---
+++ src/utils/solarSystem.js
@@ -0,0 +1,31 @@
+
+/*
+
+Usage examples:
+
+const mars = solarSystem.getMars();
+mars.compute(Date.now());
+const coordinates = mars.getHeliocentricCartesianCoordinates();
+
+const solarSystem = new SolarSystem();
+solarSystem.compute(Date.now());
+const x = solarSystem.getMars().getCartesianCoordinates().x;
+
+const solarSystem = new SolarSystem(Date.now());
+const jd = solarSystem.getJulianDate();
+
+*/
+
+export default class SolarSystem {
+  constructor() {
+
+  }
+
+  compute(date) {
+
+  }
+
+  getJulianDate() {
+    return 0.5;
+  }
+}
