8bf553276c1a7c3b690d897aae7181715f46979f
Michael Tauer <michael.tauer@futurice.com>
2017-09-28 23:37:35 +0200

    Create skeleton for the solar system page

---
+++ src/features/solarSystem/SolarSystemPage.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+class SolarSystemPage extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>SolarSystemPage</h1>
+        <p>Julian Date: </p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystemPage;
