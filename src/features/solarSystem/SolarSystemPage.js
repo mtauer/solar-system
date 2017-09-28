cd711dd2ee322350b8cb2a3752d7d6e17a1a4b12
Michael Tauer <michael.tauer@futurice.com>
2017-09-29 00:32:52 +0200

    Show unix timestamp on solar system page

--- src/features/solarSystem/SolarSystemPage.js
+++ src/features/solarSystem/SolarSystemPage.js
@@ -1,14 +1,28 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class SolarSystemPage extends PureComponent {
   render() {
+    const { timestamp } = this.props;
     return (
       <div>
         <h1>SolarSystemPage</h1>
-        <p>Julian Date: </p>
+        <p>Unix timestamp: {timestamp}</p>
       </div>
     );
   }
 }
 
-export default SolarSystemPage;
+const propTypes = {
+  timestamp: PropTypes.number.isRequired,
+};
+SolarSystemPage.propTypes = propTypes;
+
+const mapStateToProps = state => ({
+  timestamp: state.solarSystem.timestamp,
+});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SolarSystemPage);
