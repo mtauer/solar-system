c9e11f442a18018ad26c8b007fe1883061425ddf
Michael Tauer <michael.tauer@futurice.com>
2017-09-30 17:50:12 +0200

    Show Julian date on solar system page

--- src/features/solarSystem/SolarSystemPage.js
+++ src/features/solarSystem/SolarSystemPage.js
@@ -1,15 +1,17 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { dateToJulianDate } from '../../utils/julianDate';
 
 class SolarSystemPage extends PureComponent {
   render() {
-    const { timestamp, dateString } = this.props;
+    const { timestamp, dateString, julianDate } = this.props;
     return (
       <div>
         <h1>SolarSystemPage</h1>
         <p>Unix timestamp: {timestamp}</p>
-        <p>{dateString}</p>
+        <p>{dateString} (Gregorian Calendar)</p>
+        <p>Julian date: {julianDate}</p>
       </div>
     );
   }
@@ -18,15 +20,18 @@ class SolarSystemPage extends PureComponent {
 const propTypes = {
   timestamp: PropTypes.number.isRequired,
   dateString: PropTypes.string.isRequired,
+  julianDate: PropTypes.number.isRequired,
 };
 SolarSystemPage.propTypes = propTypes;
 
 const mapStateToProps = (state) => {
   const { timestamp } = state.solarSystem;
   const dateString = new Date(timestamp).toString();
+  const julianDate = dateToJulianDate(timestamp);
   return {
     timestamp,
     dateString,
+    julianDate,
   };
 };
 
